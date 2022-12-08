b2616a0c489fb792f2da80bf9e8be8fa001924c9
Erinneria <903744524@qq.com>
2022-12-09 00:41:42 +0800

    Site updated: 2022-12-09 00:41:39

--- js/particlex.js
+++ js/particlex.js
@@ -1,37 +1,56 @@
 hljs.highlightAll();
-var pre_blocks = document.getElementsByTagName("pre");
-for (var i = 0; i < pre_blocks.length; i++) {
-    var code_block_class = pre_blocks[i].firstChild.className;
-    var language = code_block_class;
-    if (language == "") language = "Text";
-    var language_html = "<div class='language'>" + language + "</div>";
-    pre_blocks[i].innerHTML += language_html;
-}
+hljs.configure({ ignoreUnescapedHTML: true });
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 const App = Vue.createApp({
     data() {
         return {
-            show_page: false,
-            menu_show: false,
-            card_top: 100,
-            bar_local: 0,
+            showpage: false,
+            menushow: false,
+            cardtop: 100,
+            barlocal: 0,
         };
+    },
+    created() {
+        let that = this;
+        window.addEventListener("load", () => {
+            that.showpage = true;
+            document.getElementById("loading").style.opacity = 0;
+            document.getElementById("loading").style.visibility = "hidden";
+        });
     },
     mounted() {
         if (document.getElementById("home-head"))
             document.getElementById("menu").className += " menu-color";
-        if (document.getElementsByClassName("article")[0])
-            document.body.style.backgroundColor = "#fff";
         window.addEventListener("scroll", this.handleScroll, true);
-    },
-    created() {
-        var that = this;
-        window.onload = function () {
-            that.show_page = true;
-            document.getElementById("loading").style.opacity = 0;
-            setTimeout(function () {
-                document.getElementById("loading").style.display = "none";
-            }, 300);
-        };
+        let codes = document.getElementsByTagName("pre");
+        let copying = false;
+        for (let code of codes) {
+            const lang =
+                code?.firstChild.className.split(/\s+/).filter(x => {
+                    return x != "sourceCode";
+                })[0] || "text";
+            let content = document.createElement("div");
+            content.classList.add("code-content");
+            content.innerHTML = code.innerHTML;
+            let language = document.createElement("div");
+            language.classList.add("language");
+            language.innerHTML = lang;
+            let copycode = document.createElement("div");
+            copycode.classList.add("copycode");
+            copycode.innerHTML =
+                '<i class="fa-solid fa-copy"></i><i class="fa-solid fa-clone"></i>';
+            copycode.addEventListener("click", async function () {
+                if (copying) return;
+                copying = true;
+                copycode.classList.add("copied");
+                await navigator.clipboard.writeText(this.parentElement.firstChild.innerText);
+                await sleep(1000);
+                copycode.classList.remove("copied");
+                copying = false;
+            });
+            code.innerHTML = "";
+            code.append(content, language, copycode);
+        }
     },
     methods: {
         home_click() {
@@ -41,17 +60,26 @@ const App = Vue.createApp({
             });
         },
         handleScroll() {
-            var new_local = document.documentElement.scrollTop;
-            var menu = document.getElementById("menu");
-            var that = this;
-            if (this.bar_local < new_local) {
+            let newlocal = document.documentElement.scrollTop;
+            let menu = document.getElementById("menu");
+            let wrap = document.getElementById("home-posts-wrap");
+            let footer = document.getElementById("footer");
+            let that = this;
+            if (this.barlocal < newlocal) {
                 menu.className = "hidden-menu";
-                that.menu_show = false;
+                that.menushow = false;
             } else menu.className = "show-menu";
-            if (document.getElementById("home-posts-wrap"))
-                if (new_local <= window.innerHeight - 100)
-                    menu.className += " menu-color";
-            this.bar_local = new_local;
+            if (wrap) {
+                if (newlocal <= window.innerHeight - 100) menu.className += " menu-color";
+                if (newlocal <= 400) {
+                    wrap.style.top = -newlocal / 5 + "px";
+                    footer.style.top = 150 - newlocal / 5 + "px";
+                } else if (wrap.style.top != "-80px" || footer.style.top != "70px") {
+                    wrap.style.top = "-80px";
+                    footer.style.top = "70px";
+                }
+            }
+            this.barlocal = newlocal;
         },
     },
 });
